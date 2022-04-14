e0bda8f81948845a4666dc16cd9485e6e94c0cde
William-Fernandes252 <william.winchester1967@gmail.com>
2022-04-14 20:20:30 -0300

    Fix context keys in template tests

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -81,15 +81,15 @@ class SetUp(test.TestCase):
 class TemplateTestCase(SetUp):
 
     def test_index(self):
-        """Check index page"""
+        """Test index page"""
         c = test.Client()
         response = c.get("/auctions/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.context["listings"]), 5)
+        self.assertEqual(len(response.context["page_obj"]), 5)
             
             
     def test_invalid_listing_page(self):
-        """Check invalid listing page"""
+        """Test invalid listing page"""
         max_id = models.Listing.objects.all().aggregate(Max("id"))["id__max"]
         c = test.Client()
         response = c.get(f"listings/{max_id + 1}")
