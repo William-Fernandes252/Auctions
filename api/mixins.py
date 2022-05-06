42c659b769e0265eb9c0547b22c5a92553909b52
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-05 23:50:24 -0300

    Fix formatting

--- api/mixins.py
+++ api/mixins.py
@@ -1,16 +1,30 @@
 from auctions.models import Listing
+from rest_framework import viewsets
 
 
-class ListingSerializerMixin:    
+class MultipleSerializersMixin:
+
+    serializer_classes = {
+        'default': None,
+    }
+
+    def get_serializer_class(self):
+        """Instantiates and returns the serializer for the given action.
+        """
+        return self.serializer_classes.get(
+            self.action, self.serializer_classes.get('default'))
+
+
+class ListingSerializerMixin:
     def get_category(self, obj):
         if not hasattr(obj, 'id') or not isinstance(obj, Listing):
             return None
         return obj.category.name
-    
+
 
 class ListingQuerysetMixin:
     queryset = Listing.objects.active()
-    
+
     def get_queryset(self):
         queryset = super().get_queryset()
         q = self.request.GET.get('q')
@@ -24,7 +38,7 @@ class ListingQuerysetMixin:
 
 class UserListingsQuerysetMixin(ListingQuerysetMixin):
     queryset = Listing.objects.all()
-    
+
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(author=self.request.user)
