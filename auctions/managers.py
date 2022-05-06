9d6a043933cb47bf6e8d924aba4bf86efc23d6c0
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-05 23:14:15 -0300

    Fix formatting

--- auctions/managers.py
+++ auctions/managers.py
@@ -6,10 +6,10 @@ from datetime import datetime
 class ListingQuerySet(models.QuerySet):
     def is_public(self):
         return self.filter(public=True)
-    
+
     def search(self, query, user=None):
         lookup = (
-            Q(title__icontains=query) |\
+            Q(title__icontains=query) |
             Q(description__icontains=query)
         )
         queryset = self.is_public().filter(lookup)
@@ -17,12 +17,11 @@ class ListingQuerySet(models.QuerySet):
             user_qs = self.filter(user=user)
             queryset = (queryset | user_qs).distinct()
         return queryset
-    
-    
+
     def active(self):
-        queryset = self.is_public().filter(ended_manually=False, end_time__gte=datetime.now())
+        queryset = self.is_public().filter(
+            ended_manually=False, end_time__gte=datetime.now())
         return queryset
-        
-    
+
     def from_category(self, category):
         return self.active().filter(category__name=category)
