c5b52b5c90f270953b6036ec546791c80fade49d
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-07 10:57:06 -0300

    Implement custom permissions for listings and questions viewsets

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,26 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
-    
+class ListingPermission(permissions.IsAuthenticatedOrReadOnly):
+
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(
+            view.action in ('update', 'partial_update') and
+            request.user == obj.author
+        )
+
+
+class QuestionPermission(permissions.IsAuthenticatedOrReadOnly):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(
+            view.action in ('update', 'partial_update') and
+            request.user == obj.user
+        ) or bool(
+            view.action == 'answer' and
+            request.user == obj.listing.author
+        )
