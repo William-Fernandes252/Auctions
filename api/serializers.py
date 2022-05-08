38d86a6716c58c23b3780bd6e2d004787cd79ec6
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-08 12:47:03 -0300

    Implement custom hyperlink identity field to handle URIs with multiple parameters

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,36 @@
-from rest_framework import serializers, exceptions
+from rest_framework import serializers, exceptions, relations, reverse as uri
 from rest_framework_extensions import serializers as extended_serializers
 from auctions import models
 from authentication import models as auth_models, serializers as auth_serializers
 from . import mixins
+
+
+class ParameterisedHyperlinkedIdentityField(
+        relations.HyperlinkedIdentityField):
+    """
+    Represents the instance, or a property on the instance, using
+    hyperlinking.
+
+    lookup_fields is a tuple of tuples of the form: 
+    ('model_field', 'url_param')
+    """
+    lookup_fields = (('pk', 'pk'),)
+
+    def __init__(self, *args, **kwargs):
+        self.lookup_fields = kwargs.pop('lookup_fields', self.lookup_fields)
+        return super().__init__(*args, **kwargs)
+
+    def get_url(self, obj, view_name, request, format):
+        kwargs = {}
+        for model_field, url_param in self.lookup_fields:
+            attr = obj
+            for field in model_field.split('.'):
+                attr = getattr(attr, field)
+            kwargs[url_param] = attr
+        return uri.reverse(view_name,
+                           kwargs=kwargs,
+                           request=request,
+                           format=format)
 
 
 class BidAbstractSerializer(serializers.ModelSerializer):
@@ -18,7 +46,9 @@ class BidAbstractSerializer(serializers.ModelSerializer):
         ]
 
 
-class ListingAbstractSerializer(serializers.ModelSerializer):
+class ListingAbstractSerializer(serializers.ModelSerializer,
+                                mixins.MultipleSerializersMixin):
+
     url = serializers.HyperlinkedIdentityField(
         view_name='listing-detail',
         lookup_field='pk',
@@ -30,13 +60,22 @@ class ListingAbstractSerializer(serializers.ModelSerializer):
         model = models.Listing
         fields = [
             'title',
+            'description',
+            'category',
+            'initial_price',
+            'duration',
+            'public',
             'current_bid',
             'url',
             'end_time',
         ]
+        extra_kwargs = {
+            'duration': {'write_only': True},
+            'end_time': {'read_only': True}
+        }
 
 
-class BidListSerializer(serializers.ModelSerializer):
+class BidSerializer(serializers.ModelSerializer):
     on = serializers.DateTimeField(source='creation_time', read_only=True)
     user = auth_serializers.UserSerializer(read_only=True)
     listing = ListingAbstractSerializer(read_only=True)
@@ -51,14 +90,13 @@ class BidListSerializer(serializers.ModelSerializer):
         ]
 
 
-class ListingListSerializer(
+class ListingSerializer(
         mixins.ListingSerializerMixin,
         serializers.ModelSerializer):
 
-    url = serializers.HyperlinkedIdentityField(
-        view_name='listing-detail',
-        lookup_field='pk',
-        lookup_url_kwarg='parent_lookup_author'
+    url = ParameterisedHyperlinkedIdentityField(
+        view_name='dashboard-listings-detail',
+        lookup_fields=(('author.pk', 'parent_lookup_author'), ('pk', 'pk')),
     )
     current_bid = BidAbstractSerializer(source='bids.first', read_only=True)
 
@@ -74,7 +112,7 @@ class ListingListSerializer(
         ]
 
 
-class ListingDetailsSerializer(
+class ListingDetailSerializer(
         mixins.ListingSerializerMixin,
         serializers.ModelSerializer):
 
@@ -89,22 +127,11 @@ class ListingDetailsSerializer(
 
     class Meta:
         model = models.Listing
-        fields = [
-            'id',
-            'author',
-            'title',
-            'description',
-            'category',
-            'initial_price',
-            'current_bid',
-            'all_bids',
-            'creation_time',
-            'end_time',
-            'public',
-        ]
+        exclude = ['winner', 'image', 'watchers', 'public']
+        read_only_fields = ['id', 'author', 'creation_time', 'end_time']
 
 
-class ListingCreationSerializer(serializers.ModelSerializer):
+class ListingCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Listing
@@ -118,7 +145,7 @@ class ListingCreationSerializer(serializers.ModelSerializer):
         ]
 
 
-class ListingEditSerializer(
+class ListingUpdateSerializer(
         extended_serializers.PartialUpdateSerializerMixin,
         serializers.ModelSerializer,
         mixins.ListingSerializerMixin):
@@ -181,7 +208,7 @@ class AnswerCreationSerializer(serializers.ModelSerializer):
 
 class QuestionSerializer(serializers.ModelSerializer):
     on = serializers.DateTimeField(source='time', read_only=True)
-    user = auth_serializers.UserSerializer(read_only=True)
+    user = serializers.CharField(source='user.get_full_name', read_only=True)
     answer = AnswerSerializer(read_only=True)
 
     class Meta:
@@ -207,15 +234,21 @@ class DashboardSerializer(serializers.HyperlinkedModelSerializer):
             'bids_count',
             'bids',
             'wins',
-            'questions',
             'watchlist',
         ]
         read_only_fields = fields
         extra_kwargs = {
-            'listings': {'view_name': 'dashboard-listings-list'},
-            'bids': {'view_name': 'dashboard-bids-list'},
+            'listings': {
+                'view_name': 'dashboard-listings-list',
+                'lookup_field': 'pk',
+                'lookup_url_kwarg': 'parent_lookup_author'
+            },
+            'bids': {
+                'view_name': 'dashboard-bids-list',
+                'lookup_field': 'pk',
+                'lookup_url_kwarg': 'parent_lookup_user'
+            },
             'wins': {'view_name': 'dashboard-wins'},
-            'questions': {'view_name': 'dashboard-questions-list'},
             'watchlist': {'view_name': 'dashboard-watchlist'},
         }
 
