454758a26d3b7dd79aa0a5c7e77093df872c1d55
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-08 18:38:09 -0300

    Delete user queryset mixin, which is not being used anymore

--- api/mixins.py
+++ api/mixins.py
@@ -1,5 +1,6 @@
-from auctions.models import Listing
-from rest_framework import viewsets
+from auctions import models
+from authentication import models as auth_models
+from rest_framework import permissions
 
 
 class MultipleSerializersMixin:
@@ -17,7 +18,7 @@ class MultipleSerializersMixin:
 
 class ListingSerializerMixin:
     def get_category(self, obj):
-        if not hasattr(obj, 'id') or not isinstance(obj, Listing):
+        if not hasattr(obj, 'id') or not isinstance(obj, models.Listing):
             return None
         return obj.category.name
 
@@ -34,11 +35,3 @@ class ListingQuerysetMixin:
             if category is not None:
                 queryset = queryset.from_category(category)
         return queryset
-
-
-class UserListingsQuerysetMixin(ListingQuerysetMixin):
-    queryset = Listing.objects.all()
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.filter(author=self.request.user)
