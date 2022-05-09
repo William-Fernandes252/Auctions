2c44087b0de92959a598d8610d6f7384cfec0273
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-08 23:17:13 -0300

    Register viewsets and remove hardcoded urls

--- api/urls.py
+++ api/urls.py
@@ -1,41 +1,30 @@
-from django.urls import path
-from . import views, viewsets
-from rest_framework import routers
+from . import viewsets
+from rest_framework_extensions import routers
 
 
-router = routers.DefaultRouter()
-router.register(r'listings', viewsets.ListingViewSet)
+router = routers.ExtendedDefaultRouter()
 router.register(r'bids', viewsets.BidsViewSet)
 
-urlpatterns = [
-    path(
-        "listings/<int:listing_pk>/questions/<int:question_pk>/answer/", 
-        views.answer_question_view, 
-        name="answer-question"
-    ),
-    path(
-        "user/", 
-        views.user_home_api_view, 
-        name="user-home"
-    ),
-    path(
-        "user/watchlist/", 
-        views.user_watchlist_api_view, 
-        name="listing-watchlist"
-    ),
-    path(
-        "user/listings/", 
-        views.user_listing_list_view, 
-        name="user-listing-list"
-    ),
-    path(
-        "user/bids/",
-        views.user_bid_list_view,
-        name="user-bid-list"
-    ),
-    path(
-        "user/listings/<int:pk>/", 
-        views.user_listing_details_view, 
-        name="user-listing-details"
-    ),
-] + router.urls
+listing_router = router.register(r'listings', viewsets.ListingViewSet)
+listing_router.register(r'bids',
+                        viewsets.ListingBidsViewSet,
+                        basename='listing-bids',
+                        parents_query_lookups=['listing'])
+listing_router.register(r'questions',
+                        viewsets.ListingQuestionsViewSet,
+                        basename='listing-questions',
+                        parents_query_lookups=['listing'])
+
+dashboard_router = router.register(r'dashboard',
+                                   viewsets.DashboardViewSet,
+                                   basename='dashboard')
+dashboard_router.register(r'listings',
+                          viewsets.DashboardListingsViewSet,
+                          basename='dashboard-listings',
+                          parents_query_lookups=['author'])
+dashboard_router.register(r'bids',
+                          viewsets.DashboardBidsViewSet,
+                          basename='dashboard-bids',
+                          parents_query_lookups=['user'])
+
+urlpatterns = router.urls
