3fa4e5237e3bc53981b575db7c10be86db1f52df
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-08 23:18:41 -0300

    Implement viewsets for listings, bids, questions and dashboard

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,88 +1,45 @@
-from rest_framework import viewsets, mixins, decorators, status, generics
+from rest_framework import (
+    viewsets,
+    mixins,
+    decorators,
+    status,
+    generics,
+)
 from rest_framework.response import Response
-from . import serializers
+from rest_framework_extensions import mixins as extension_mixins
+from . import serializers, permissions as api_permissions
 from rest_framework import permissions
 from . import mixins as api_mixins
 from auctions import models
+from authentication import models as auth_models
+
+from django.shortcuts import get_object_or_404
 
 
-class ListingViewSet(api_mixins.MultipleSerializersMixin,
-                     viewsets.GenericViewSet,
+class BidsViewSet(viewsets.GenericViewSet,
+                  mixins.ListModelMixin):
+
+    queryset = models.Bid.objects.all()
+    serializer_class = serializers.BidSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class ListingViewSet(extension_mixins.DetailSerializerMixin,
+                     api_mixins.ListingQuerysetMixin,
                      mixins.ListModelMixin,
                      mixins.RetrieveModelMixin,
-                     mixins.CreateModelMixin):
+                     mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     viewsets.GenericViewSet):
 
     queryset = models.Listing.objects.active()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    serializer_classes = {
-        'list': serializers.ListingAbstractSerializer,
-        'retrieve': serializers.ListingDetailsSerializer,
-        'create': serializers.ListingCreationSerializer,
-        'bids': serializers.BidAbstractSerializer,
-        'create_bid': serializers.BidAbstractSerializer,
-        'questions': serializers.QuestionSerializer,
-        'pub_question': serializers.QuestionSerializer,
-    }
-
-    def get_queryset(self):
-        if 'bid' in self.action:
-            return models.Listing.objects.get(pk=self.kwargs.get('pk')).bids
-        elif 'question' in self.action:
-            return models.Listing.objects.get(pk=self.kwargs.get('pk')).questions
-
-        queryset = super().get_queryset()
-        q = self.request.GET.get('q')
-        category = self.request.GET.get('category')
-        if q is not None:
-            queryset = queryset.search(query=q)
-        if category is not None:
-            queryset = queryset.from_category(category)
-        return queryset
+    queryset_detail = queryset.prefetch_related('bids')
+    permission_classes = (api_permissions.ListingPermission,)
+    serializer_class = serializers.ListingAbstractSerializer
+    serializer_detail_class = serializers.ListingDetailSerializer
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    @decorators.action(
-        detail=True,
-        methods=['get'],
-        pagination_class=None,
-        name='Listing Bids',
-    )
-    def bids(self, request, pk=None):
-        serializer = self.get_serializer(self.get_queryset(), many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @bids.mapping.post
-    @decorators.permission_classes([permissions.IsAuthenticated])
-    def create_bid(self, request, pk=None):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(
-            user=self.request.user,
-            listing=models.Listing.objects.get(pk=pk)
-        )
-        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-
-    @decorators.action(
-        detail=True,
-        methods=['get'],
-        name='Listing Questions',
-        pagination_class=None,
-    )
-    def questions(self, request, pk=None):
-        serializer = self.get_serializer(self.get_queryset(), many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @questions.mapping.post
-    @decorators.permission_classes([permissions.IsAuthenticated])
-    def pub_question(self, request, pk=None):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(
-            user=self.request.user,
-            listing=models.Listing.objects.get(pk=pk)
-        )
-        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
 
     @decorators.action(
         detail=False,
@@ -100,46 +57,117 @@ class ListingViewSet(api_mixins.MultipleSerializersMixin,
         return Response(status=status.HTTP_200_OK)
 
 
-listing_list_view = ListingViewSet.as_view(
-    {'get': 'list'}
-)
-listing_detail_view = ListingViewSet.as_view(
-    {'get': 'retrieve'}
-)
-listing_create_view = ListingViewSet.as_view(
-    {'post': 'create'}
-)
-listing_bids_view = ListingViewSet.as_view(
-    {'get': 'bids', 'post': 'create_bid'}
-)
-listing_questions_view = ListingViewSet.as_view(
-    {'get': 'questions', 'post': 'pub_question'}
-)
+class ListingBidsViewSet(extension_mixins.NestedViewSetMixin,
+                         mixins.CreateModelMixin,
+                         mixins.RetrieveModelMixin,
+                         mixins.DestroyModelMixin,
+                         BidsViewSet):
+
+    serializer_class = serializers.BidAbstractSerializer
+    pagination_class = None
+
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user,
+            listing=models.Listing.objects.get(
+                pk=self.kwargs.get('parent_lookup_listing')
+            )
+        )
 
 
-class BidsViewSet(viewsets.GenericViewSet,
-                  mixins.ListModelMixin,
-                  mixins.DestroyModelMixin):
+class ListingQuestionsViewSet(extension_mixins.NestedViewSetMixin,
+                              viewsets.ModelViewSet):
 
-    queryset = models.Bid.objects.all()
-    serializer_class = serializers.BidListSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    queryset = models.Question.objects.all()
+    serializer_class = serializers.QuestionSerializer
+    permission_classes = (api_permissions.QuestionPermission,)
+    pagination_class = None
 
-
-class DashboardViewSet(viewsets.ViewSet):
-    permission_classes = (permissions.IsAuthenticated,)
-    serializer_classes = {
-        'home': serializers.DashboardSerializer,
-    }
-
-    def get_queryset(self):
-        if self.action == 'home':
-            return self.request.user
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user,
+            listing=models.Listing.objects.get(
+                pk=self.kwargs.get('parent_lookup_listing')
+            )
+        )
 
     @decorators.action(
-        detail=False,
+        detail=True,
+        methods=['post'],
+        serializer_class=serializers.AnswerSerializer,
+        lookup_field='pk',
+        lookup_url_kwarg='parent_lookup_listing'
+    )
+    def answer(self, request, **kwargs):
+        question = get_object_or_404(models.Question, pk=kwargs.get('pk'))
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.check_object_permissions(request, question)
+        question.answer = serializer.save(author=request.user)
+        question.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class DashboardViewSet(api_mixins.MultipleSerializersMixin,
+                       viewsets.GenericViewSet):
+
+    queryset = auth_models.User.objects.all()
+    permission_classes = (api_permissions.DashboardPermission,)
+    serializer_classes = {
+        'home': serializers.DashboardSerializer,
+        'watchlist': serializers.ListingSerializer,
+        'wins': serializers.ListingSerializer
+    }
+
+    @decorators.action(
+        detail=True,
         methods=['get'],
-        url_path='',
+        name="Dashboard Home"
     )
     def home(self, request, **kwargs):
-        pass
+        return Response(self.get_serializer(request.user).data,
+                        status=status.HTTP_200_OK)
+
+    @decorators.action(
+        detail=True,
+        methods=['get'],
+    )
+    def watchlist(self, request, **kwargs):
+        queryset = request.user.watchlist.active()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @decorators.action(
+        detail=True,
+        methods=['get']
+    )
+    def wins(self, request, **kwargs):
+        return Response(self.get_serializer(request.user.wins).data,
+                        status=status.HTTP_200_OK)
+
+
+class DashboardListingsViewSet(extension_mixins.NestedViewSetMixin,
+                               extension_mixins.DetailSerializerMixin,
+                               api_mixins.ListingQuerysetMixin,
+                               mixins.ListModelMixin,
+                               mixins.RetrieveModelMixin,
+                               mixins.UpdateModelMixin,
+                               viewsets.GenericViewSet):
+
+    queryset = models.Listing.objects.all()
+    queryset_detail = queryset.prefetch_related('bids')
+    permission_classes = (
+        api_permissions.DashboardPermission,
+        api_permissions.ListingPermission,
+    )
+    serializer_class = serializers.ListingSerializer
+    serializer_detail_class = serializers.ListingUpdateSerializer
+
+
+class DashboardBidsViewSet(extension_mixins.NestedViewSetMixin,
+                           BidsViewSet):
+    permission_classes = (api_permissions.DashboardPermission,)
